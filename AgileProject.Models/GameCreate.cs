7d2a278663189bf885ce47d0ccdc576020189b22
Mitchell Pickard <mitchell.pickard93@gmail.com>
2021-10-21 15:03:34 -0400

    recommit due to previous error

--- AgileProject.Models/GameCreate.cs
+++ AgileProject.Models/GameCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace AgileProject.Models
 {
     public class GameCreate
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter a title.")]
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
     }
 }
