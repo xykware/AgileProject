8c822d138003441e0202a9b08ad85be49b5a0679
Mitchell Pickard <mitchell.pickard93@gmail.com>
2021-10-21 14:59:42 -0400

    Added Game Class, Game Create, Game Detail

---
+++ AgileProject.Models/GameDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProject.Models
+{
+    class GameDetail
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter a title.")]
+        public string Title { get; set; }
+
+        public string Comment { get; set; }
+
+        [Display(Name = "Date Entered")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date Modified")]
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
