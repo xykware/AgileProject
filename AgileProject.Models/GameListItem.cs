e7e862cbf773e2f759ce5de99fe3e6b4d11e0ff1
Joe.Micucci <micucci.joseph@gmail.com>
2021-10-21 15:10:04 -0400

    Updated GameDetail. Added GameEdit, GameListItem

--- AgileProject.Models/GameListItem.cs
+++ AgileProject.Models/GameListItem.cs
@@ -7,18 +7,11 @@ using System.Threading.Tasks;
 
 namespace AgileProject.Models
 {
-    class GameDetail
+    public class GameListItem
     {
-        [Required]
-        [MinLength(1, ErrorMessage = "Please enter a title.")]
+        public int GameId { get; set; }
         public string Title { get; set; }
-
-        public string Comment { get; set; }
-
         [Display(Name = "Date Entered")]
         public DateTimeOffset CreatedUtc { get; set; }
-
-        [Display(Name = "Date Modified")]
-        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
