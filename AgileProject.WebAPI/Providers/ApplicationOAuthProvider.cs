bac9bffe8899755513d087806a739a97ca92ac15
DavidBlomeyer <89857190+DavidBlomeyer@users.noreply.github.com>
2021-10-22 10:00:07 -0400

    links fixed

--- AgileProject.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ AgileProject.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using AgileProject.WebAPI.Models;
+using AgileProject.Data;
 
 namespace AgileProject.WebAPI.Providers
 {
